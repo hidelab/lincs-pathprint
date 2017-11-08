37ddc6bcad229dfe04f50a433faf52961b3644ad
Sokratis Kariotis <sokratiskariotis@gmail.com>
2017-11-08 16:08:54 +0000

    updated gctx_to_SCE.R

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 New pipeline for pathprinting LINCS data
 
 ## Scripts
-1. **gctx_to_SCE.R** : Download/read the Level 3 GSE from LINCS (GSE70138_Broad_LINCS_Level3_INF_mlr12k_n345976x12328_2017-03-06.gctx) and create the sampleSet.RData file that holds all expreriments (each column of .gctx matrix) to be used in the next step. The downloaded experiments are split into 15 chunks to increase download nad updating speed as well as be ready for the multi-thread fingerprinting in the following scripts.
+1. **gctx_to_SCE.R** : Script to download the Level 3 GSE from LINCS (GSE70138_Broad_LINCS_Level3_INF_mlr12k_n345976x12328_2017-03-06.gctx) and split them in a number of chunks (adjustable by number_of_chunks variable). Before saving to an equal number of matrices, the gene IDs are updated using the entrezUpdate.R script from the package GMAfunctions. Finally, the script initiates a number of cores each one handling running the gctx2fingerprint.R script to produce the experiment fingerprints.
+
+2. **gctx2fingerprint.R** : Script that based on a gctx partial matrix produces the expreriment fingerprints
--- gctx_to_SCE.R
+++ gctx_to_SCE.R
@@ -3,12 +3,14 @@
 # Author: Sokratis Kariotis
 # Status: v1
 # Timestamp: 07.11.2017
-# Script to download or read the Level 3 GSE from LINCS 
-# (GSE70138_Broad_LINCS_Level3_INF_mlr12k_n345976x12328_2017-03-06.gctx) and create
-# the sampleSet.RData file that holds the gene expression of all genes in one expreriment
-# (per column of .gctx matrix). Then the sampleSet object  gets split up and passed to 
-# the fingerprinting scripts. Currently split into 14 parts as the fingerprinting script 
-# is passed to 14 cores.
+# Script to download the Level 3 GSE from LINCS 
+# (GSE70138_Broad_LINCS_Level3_INF_mlr12k_n345976x12328_2017-03-06.gctx) and 
+# split them in a number of chunks (adjustable by number_of_chunks variable).
+# Before saving to an equal number of matrices, the gene IDs are updated using
+# the entrezUpdate.R script from the package GMAfunctions. Finally, the script 
+# initiates a number of cores each one handling running the gctx2fingerprint.R
+# script to produce the experiment fingerprints.
+
 
 # For this script we need:
 # > (optional) The .gctx matrix downloaded here "~/LINCS project/LINCS specific scripts/gctx_Folder"
@@ -17,6 +19,10 @@
 # load library tha handles .gctx files
 library(cmapR)
 library(GMAfunctions)
+
+# Select the number of chunks
+number_of_chunks <- 15
+
 
 # Check if the .gctx matrix is in the gctx_Folder. If not download manually and add it.
 if(!file.exists("~/LINCS project/LINCS specific scripts/gctx_Folder/GSE70138_Broad_LINCS_Level3_INF_mlr12k_n345976x12328.gctx",showWarnings = TRUE)[1])
@@ -29,99 +35,55 @@ gene_count <- length(parse.gctx("~/LINCS project/LINCS specific scripts/gctx_Fol
                    cid=1:1, matrix_only = TRUE)@rid)
 
 # The experiment files are here
-exp_path <- "~/LINCS project/LINCS specific scripts/gctx_experiments/"
+gctx_exp_path <- "~/LINCS project/LINCS specific scripts/gctx_experiments/"
 
 # Locate the Entrez history file to be used for updating
 historyFile <- "~/LINCS project/LINCS specific scripts/Entrez Gene History/EntrezHistoryList.RData"
 
 # We have to split the matrix column-wise/experiment-wise since its 12.6 Gb altogether
-complete_matrix <- matrix(nrow = gene_count, ncol = experiments_count/10)
-chunk <- as.integer(experiments_count / 1  )
+chunk <- as.integer(experiments_count / number_of_chunks)
 
-for (exp in 1:experiments_count) {
-        temp_exp <- parse.gctx("~/LINCS project/LINCS specific scripts/gctx_Folder/GSE70138_Broad_LINCS_Level3_INF_mlr12k_n345976x12328.gctx",
-                         cid=exp:exp)@mat
-        
-        # Update Entrez Ids
-        new <- entrezUpdate(rownames(temp_exp), historyFile = historyFile)
-        old <- rownames(temp_exp)
-        print(Reduce(setdiff, strsplit(c(new, old), split = "")))
-        
-        # rownames(temp_exp)<-entrezUpdate(rownames(temp_exp), historyFile = historyFile)
-        
-        exp_name <- gsub(":", "_", colnames(temp_exp))
-        save_name <- paste(exp_path,exp_name,".RData", sep="")
-        save(temp_exp, file = save_name)
-        print(paste("Downloaded experiment ",exp_name))
-        print(paste(exp," experiments complete."))
-}
+# gctx matrices are located here
+exp_path <- "~/LINCS project/LINCS specific scripts/gctx_Folder/gctx_matrix_"
 
+sampleset <- vector("list", number_of_chunks)
+sampleset_count <- 1
 
-for (matrix_chunk in seq(1,experiments_count/10, by = chunk)){
+for (matrix_chunk in seq(1,experiments_count, by = chunk)){
     current_start <- matrix_chunk
 
     # Check if its last loop
     if (matrix_chunk + chunk > experiments_count) current_end <- experiments_count
     else current_end <- matrix_chunk + chunk - 1
 
-    print(paste("Select ", current_start, " to ", current_end ))
-    temp_mat <- parse.gctx("~/LINCS project/LINCS specific scripts/gctx_Folder/GSE70138_Broad_LINCS_Level3_INF_mlr12k_n345976x12328.gctx",
+    partial_gctx_matrix <- parse.gctx("~/LINCS project/LINCS specific scripts/gctx_Folder/GSE70138_Broad_LINCS_Level3_INF_mlr12k_n345976x12328.gctx",
                      cid=current_start:current_end, matrix_only = TRUE)@mat
+    
+    # Updating the Entrez IDs
+    rownames(partial_gctx_matrix)<-entrezUpdate(rownames(partial_gctx_matrix), historyFile = historyFile)
+    print("Entrez IDs updated.")
+    
+    # Fill the sample set
+    sampleset[[sampleset_count]] <- colnames(partial_gctx_matrix)
+    
+    save_name <- paste(exp_path,current_start,"-",current_end,".RData", sep="")
+    save(partial_gctx_matrix, file = save_name)
+    print(paste("Downloaded and saved experiments ", current_start, " to ", current_end, " in ", save_name))
+    sampleset_count <- sampleset_count + 1
+    
+    for (e in 1:length(colnames(partial_gctx_matrix))) {
 
-    # Add to the complete matrix
-    complete_matrix <- cbind(complete_matrix,temp_mat)
-    print(paste("Added experiments ", current_start, " to ", current_end , " to the complete matrix"))
+      expression_column <- matrix(partial_gctx_matrix[,e])
+      gene_column <- matrix(rownames(partial_gctx_matrix))
+      exp_matrix <- cbind(gene_column,expression_column)
+      
+      experiment_name <- colnames(partial_gctx_matrix)[e]
+      experiment_name <- gsub(":", "_", experiment_name)
+      full_experiment_name <- paste(gctx_exp_path,experiment_name,".RData", sep = "")
+      save(exp_matrix, file = full_experiment_name)
+    }
+    
 }
 
-# save(complete_matrix, file = "~/LINCS project/LINCS specific scripts/complete_matrix.RData")
-
-
-# load("/shared/hidelab2/shared/Sokratis/pathprint/fingerprint/data/chipframe36.rda")
-# 
-# 
-# # Use GEOmetaDB to retrieve the arrays
-# wd<-getwd()
-# library(GEOmetadb)
-# 
-# # save GEOmetadb database locally on Iceberg
-# setwd("/shared/hidelab2/shared/Sokratis/pathprint/Databases/GEOmetadb")
-# 
-# # download and extract database
-# #getSQLiteFile()
-# con <- dbConnect(SQLite(), "GEOmetadb.sqlite")
-# 
-# # Retrieve information from GEO
-# #chipnames<-names(chipframe)
-# chipnames<-"GPL7202"
-# chipvector<-vector("character", 0)
-# print("Querying GEOmetaDB")
-# for (i in 1:length(chipnames)){
-#   query<-paste("SELECT gsm FROM gsm WHERE gpl = '", chipnames[i], "'", sep = "")
-#   print(query)
-#   gsmList<-dbGetQuery(con, query)
-#   chipvector<-c(chipvector, unlist(gsmList, use.names = FALSE))
-#   }
-# 
-# save(gsmList, file = "gsmList.RData")
-# print("gsm list")
-# 
-#   
-# GEOsamples.new<-chipvector
-# dbDisconnect(con)
-# 
-# # reset working directory
-# setwd(wd)
-# 
-# # scramble sample names to avoid bias in processing time asscociated with the larger chips
-# # apportion into 12 or 14
-# print(paste(length(GEOsamples.new), "to be fingerprinted", sep = " ")) 
-# GEOsamples.rand <- sample(GEOsamples.new)
-# len <- length(GEOsamples.rand) %/% 14
-# 
-# sampleset<-vector("list", 14)
-# for (i in 1:length(sampleset)){
-# 	sampleset[[i]] <- GEOsamples.rand[(1+((i-1)*len)):(i*len)]
-# 	}
-# sampleset[[14]] <- GEOsamples.rand[(1+(13*len)):length(GEOsamples.rand)]
-# save(sampleset, file = "sampleset.RData")
-# print("Sampleset saved!")
+save(sampleset, file = "sampleset.RData")
+print("Sample set saved.")
